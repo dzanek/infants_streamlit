4b2e9e713e3e3694b258e49630cadafadf907ec8
JMajta <jmajta@MacBookProJmajta.local>
2022-03-07 16:56:14 +0100

    major: changed input to SRA

--- infants_dashboard.py
+++ infants_dashboard.py
@@ -10,251 +10,103 @@ import numpy as np
 import scipy.stats as ss
 
 import os
-st.set_page_config(layout="wide")
-st.title('Infant Gut microbiome in Russia and Finland')
 
+from utils import *
+
+#st.set_page_config(layout="wide")
+st.title('SRA study based microbial signature discovery tool')
+st.write('Sample data from Vatanen et al. 2016')
 rus_ids = [i.strip() for i in open('id_rus.srx')]
 fin_ids = [i.strip() for i in open('id_fin.srx')]
 all_ids = rus_ids+fin_ids
 
-country_map = {i:'Rosja' for i in rus_ids}
-country_map.update({i:'Finlandia' for i in fin_ids})
-
-samples_map = [i.strip().split() for i in open('samples.map')]
-samples_map = {i[1]:i[0] for i in samples_map}
-
-def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
-    """
-    Create a plot of the covariance confidence ellipse of *x* and *y*.
-
-    Parameters
-    ----------
-    x, y : array-like, shape (n, )
-        Input data.
-
-    ax : matplotlib.axes.Axes
-        The axes object to draw the ellipse into.
-
-    n_std : float
-        The number of standard deviations to determine the ellipse's radiuses.
-
-    **kwargs
-        Forwarded to `~matplotlib.patches.Ellipse`
-
-    Returns
-    -------
-    matplotlib.patches.Ellipse
-    """
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from matplotlib.patches import Ellipse
-    import matplotlib.transforms as transforms
 
 
-    if x.size != y.size:
-        raise ValueError("x and y must be the same size")
-
-    cov = np.cov(x, y)
-    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
-    # Using a special case to obtain the eigenvalues of this
-    # two-dimensionl dataset.
-    ell_radius_x = np.sqrt(1 + pearson)
-    ell_radius_y = np.sqrt(1 - pearson)
-    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
-                      facecolor=facecolor, **kwargs)
-
-    # Calculating the stdandard deviation of x from
-    # the squareroot of the variance and multiplying
-    # with the given number of standard deviations.
-    scale_x = np.sqrt(cov[0, 0]) * n_std
-    mean_x = np.mean(x)
-
-    # calculating the stdandard deviation of y ...
-    scale_y = np.sqrt(cov[1, 1]) * n_std
-    mean_y = np.mean(y)
-
-    transf = transforms.Affine2D() \
-        .rotate_deg(45) \
-        .scale(scale_x, scale_y) \
-        .translate(mean_x, mean_y)
-
-    ellipse.set_transform(transf + ax.transData)
-    return ax.add_patch(ellipse)
-
-def load_data(tax_level='P'):
-    from sklearn.preprocessing import StandardScaler
-    taxonomy_dataframe_P = pd.DataFrame()
-    for i in all_ids:
-        #print(taxonomy_dataframe_P)
-        sample = pd.read_table(f'bracken_{tax_level}/{samples_map[i]}.bracken',index_col=0)
-        sample = sample[['fraction_total_reads']]
-        sample.columns = [i]
-        if len(taxonomy_dataframe_P.columns) == 0:
-            print('....')
-            taxonomy_dataframe_P = sample.copy()
-        else:
-            taxonomy_dataframe_P = pd.merge(taxonomy_dataframe_P, sample[[i]], how='outer', suffixes=("", ""),left_index=True, right_index=True)
-    #taxonomy_dataframe_P['phylum'] = taxonomy_dataframe_P.index
-    taxonomy_dataframe_P.fillna(0.0,inplace=True)
-    taxonomy_dataframe_P.index.rename('phylum',inplace=True)
-    taxonomy_dataframe_P['mean'] = taxonomy_dataframe_P.apply(lambda x: np.average(x),axis=1)
-    taxonomy_dataframe_P.sort_values(by='mean',ascending=False,inplace=True)
-    print(len(taxonomy_dataframe_P))
-    taxonomy_dataframe_P.drop('mean',axis=1,inplace=True)
-    taxonomy_dataframe_P = taxonomy_dataframe_P.loc[(taxonomy_dataframe_P == 0.0).mean(axis=1) < 0.9]
-    print(len(taxonomy_dataframe_P))
-    return taxonomy_dataframe_P
-
-def scale_dataframe(taxonomy_dataframe_P):
-    cols = taxonomy_dataframe_P.columns
-    ind = taxonomy_dataframe_P.index
-    scaler = StandardScaler()
-    taxonomy_dataframe_P = scaler.fit_transform(taxonomy_dataframe_P)
-    taxonomy_dataframe_P = pd.DataFrame(taxonomy_dataframe_P)
-    taxonomy_dataframe_P.columns = cols
-    taxonomy_dataframe_P.index = ind
-    return taxonomy_dataframe_P
-
-def plot_barplot(data):
-    fig, phylum_bar_kraken = plt.subplots()
-    phylum_table_kraken = data
-    #phylum_table_kraken
-    phylum_table_kraken = phylum_table_kraken.T.sort_values(by=['Actinobacteria','Bacteroidetes'],ascending=[False,False]).T
-    phylum_table_kraken.T.plot(ax=phylum_bar_kraken, kind="bar", stacked=True)
-    phylum_bar_kraken.legend(list(phylum_table_kraken.index)[:5],loc=1)
-    phylum_bar_kraken.set_xticklabels([country_map[i] for i in phylum_table_kraken.columns])
-    phylum_bar_kraken.set_xlabel('Country of origin')
-    phylum_bar_kraken.set_ylabel('Fraction of taxa in the system')
-    return fig
-
-def plot_pca(data):
-    from sklearn.decomposition import PCA
-    from sklearn.preprocessing import StandardScaler
-    pcagenus = PCA(n_components=2)
-    pcagenus.fit(data)
-    var1, var2 = pcagenus.explained_variance_ratio_
-    fig, ax = plt.subplots()
-    fig, ax = plt.subplots()
-#    ax=fig.add_axes([0,0,1,1])
-    ax.scatter(pcagenus.components_[0][len(rus_ids):], pcagenus.components_[1][len(rus_ids):],linewidths=2,edgecolors='#666666',s=300,color=sns.color_palette()[0],label='Russia')
-    ax.scatter(pcagenus.components_[0][:len(rus_ids)], pcagenus.components_[1][:len(rus_ids)],linewidths=2,edgecolors='#666666',s=300,color=sns.color_palette()[1],label='Finland')
-
-    confidence_ellipse(pcagenus.components_[0][len(rus_ids):], pcagenus.components_[1][len(rus_ids):],ax,n_std=3,edgecolor=sns.color_palette()[0])
-    confidence_ellipse(pcagenus.components_[0][:len(rus_ids)], pcagenus.components_[1][:len(rus_ids)],ax,n_std=3,edgecolor=sns.color_palette()[1])
-
-    ax.legend()
-    ax.set_xlabel(f'PC1 {round(100*var1,2)}%')
-    ax.set_ylabel(f'PC2 {round(100*var2,2)}%')
-    plt.tight_layout()
-    return fig
-
-def build_model(data):
-    phylum_feature_table = data.T
-    phylum_feature_table['location'] = phylum_feature_table.index.map(lambda x: 1 if country_map[x] == 'Rosja' else 0)
-
-    from scipy import interp
-
-    from sklearn.datasets import make_classification
-    from sklearn.model_selection import RepeatedStratifiedKFold
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.metrics import roc_curve
-    from sklearn.metrics import plot_roc_curve
-    from sklearn.metrics import auc
-
-    features = list(phylum_feature_table.columns[:-1])
-    target = phylum_feature_table.columns[-1]
-    X, y = np.array(phylum_feature_table[features]), np.array(phylum_feature_table[target])
-    #print(X[[3,5,7]])
-    #print(y.shape)
-
-    cv = RepeatedStratifiedKFold(n_splits=5,n_repeats=10)
-    classifier = LogisticRegression()
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
-
-    fig, ax = plt.subplots()
-    for i, (train, test) in enumerate(cv.split(X, y)):
-        classifier.fit(X[train], y[train])
-        #print(y[test])
-        viz = plot_roc_curve(classifier, X[test], y[test],
-                             label='_nolegend_',
-                             alpha=0.0, lw=1, ax=ax)
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
+#samples_map = [i.strip().split() for i in open('samples.map')]
+#samples_map = {i[1]:i[0] for i in samples_map}
 
 
-    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color=sns.color_palette()[1],
-            label='Random', alpha=.8)
-
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(mean_fpr, mean_tpr, color=sns.color_palette()[0],
-            label=r'Average ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-            lw=2, alpha=.8)
-
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                    label=r'$\pm$ 1 std. deviation')
-
-    ax.set_xlabel('1 - specificity')
-    ax.set_ylabel('sensitivity')
-
-    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-           title="")
-    ax.legend(loc="lower right")
-
-    return fig, classifier, features
-
-def get_top_features(classifier, features):
-
-    feature_importance = abs(classifier.coef_[0])
-    feature_importance = 100.0 * (feature_importance / feature_importance.max())
-    sorted_idx = np.argsort(feature_importance)
-    pos = np.arange(sorted_idx.shape[0]) + .5
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.barh(pos[-5:], feature_importance[sorted_idx][-5:], align='center', )
-    ax.set_yticks(pos[-5:])
-    ax.set_yticklabels(np.array(features)[sorted_idx][-5:], fontsize=16)
-    ax.set_xlabel('Top 5 features')
-    #plt.ylim(39,)
-    plt.tight_layout()
-    return fig
+#data_load_state = st.text('Loading data...')
+#data = load_data(all_ids,samples_map,'P')
+#data_load_state.text("Done!")
 
 
-
-data_load_state = st.text('Loading data...')
-data = load_data('P')
-data_load_state.text("Done!")
-
-st.subheader('Raw data')
-st.write(data)
 
 col1, col2 = st.columns(2)
 
+col1.subheader('Input control samples')
 
-col1.subheader('Taxonomy profile of the cohort')
-col1.pyplot(plot_barplot(data))
-col1.markdown('The stacked bar plot sorted by the most abundant taxa in the cohort.')
+with st.spinner("Loading Control Samples"):
+    samples_a = col1.text_area('SRA run ID (newline each)', '''SRR4305340
+SRR4305202
+SRR4408106
+SRR4305207
+SRR4408087
+SRR4408201
+SRR4408082
+SRR4408085
+SRR4408049
+SRR4408188
+SRR4408152
+SRR4305204
+SRR4408203
+SRR4408123
+SRR4305334
+SRR4305376
+SRR4408149
+SRR4408059
+         '''.strip())
+    samples_a = samples_a.split()
+    print(samples_a)
+#st.success('Control loaded!')
 
-col1.subheader('PCA')
-col1.pyplot(plot_pca(data))
-col1.markdown('Principal Componen Analysis plot based on the taxonomy table.')
+with st.spinner("Loading Treatment Samples"):    
+    col2.subheader('Input treatment samples')
+    samples_b = col2.text_area('SRA run ID (newline each)', '''SRR4305272
+SRR4305403
+SRR4305496
+SRR4305406
+SRR4305488
+SRR4305368
+SRR4305407
+SRR4305409
+SRR4305356
+SRR4305286
+SRR4305515
+SRR4305483
+SRR4305414
+SRR4305508
+SRR4305032
+SRR4305361
+SRR4305408
+SRR4305285
+SRR4305175
+SRR4305396'''.strip())
+    samples_b = samples_b.split()
+    print(samples_b)
+#st.success('Treatment loaded!')
 
-rocauc, classifier, features = build_model(data)
+group_map = {i:'Control' for i in samples_a}
+group_map.update({i:'Treatment' for i in samples_b})
 
-col2.subheader('Classification performance (ROC AUC)')
-col2.pyplot(rocauc, classifier)
-col2.markdown('ROC AUC plot from 10 times repeated, 5-fold Cross Validation, with Standard Deviation from repetitions marked as grey area.')
+data = get_data_table(samples_a,samples_b)
+st.subheader('Raw data')
+st.write(data)
 
-col2.subheader('Most important features')
-col2.pyplot(get_top_features(classifier, features))
-col2.markdown('The set of features most important for making predictions by trained model.')
+st.subheader('Taxonomy profile of the cohort')
+st.pyplot(plot_barplot(data.iloc[:-1],group_map))
+st.markdown('The stacked bar plot sorted by the most abundant taxa in the cohort.')
+
+st.subheader('PCA')
+st.pyplot(plot_pca(data.iloc[:-1], len(samples_a)))
+st.markdown('Principal Componen Analysis plot based on the taxonomy table.')
+
+rocauc, classifier, features = build_model(data,group_map)
+
+st.subheader('Classification performance (ROC AUC)')
+st.pyplot(rocauc, classifier)
+st.markdown('ROC AUC plot from 10 times repeated, 5-fold Cross Validation, with Standard Deviation from repetitions marked as grey area.')
+
+st.subheader('Most important features')
+st.pyplot(get_top_features(classifier, features))
+st.markdown('The set of features most important for making predictions by trained model.')
---
+++ utils.py
@@ -0,0 +1,244 @@
+import pandas as pd
+import numpy as np
+
+import requests
+from bs4 import BeautifulSoup
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+import seaborn as sns 
+
+import streamlit as st
+    
+def build_taxonomy_table(samples,target,rank):
+    ''' '''
+    taxonomy_table = pd.DataFrame()
+    for run_id in samples:
+        run_df = pd.DataFrame()
+        url = f'https://trace.ncbi.nlm.nih.gov/Traces/sra/?run={run_id}&retmode=xml'
+        req = requests.get(url)
+        print(url)
+        Bs_data = BeautifulSoup(req.content, "xml")
+        for i in Bs_data.find('RUN').find('tax_analysis').find_all('taxon', {'rank':rank}):
+            if i.get('rank') != rank:
+                continue
+            #print(f"{i.get('name')},{i.get('rank')},{i.get('total_count')}")
+            tax_df = pd.DataFrame({run_id: float(i.get('total_count'))}, index=[i.get('name')])
+            run_df = run_df.append(tax_df)
+        taxonomy_table = pd.merge(left=taxonomy_table, right=run_df, how='outer', left_index=True, right_index=True)
+    taxonomy_table = taxonomy_table/taxonomy_table.sum()
+    taxonomy_table.loc['target'] = target    
+    return taxonomy_table
+
+@st.cache
+def get_data_table(samples_a=["SRR15021134","SRR15021145"], samples_b=["SRR15021131","SRR15021132"], rank='phylum'):
+    ''' build data table from two lists of sra accesions '''
+    
+    taxonomy_table = pd.merge(left=build_taxonomy_table(samples_a,0,rank=rank),
+                              right=build_taxonomy_table(samples_b,1,rank=rank),
+                              how='outer', left_index=True, right_index=True)
+    taxonomy_table.fillna(0.0,inplace=True)
+    taxonomy_table = taxonomy_table.loc[(taxonomy_table == 0).mean(axis=1) < 0.9]
+    return taxonomy_table
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+def load_data(all_ids, samples_map, tax_level='P'):
+    from sklearn.preprocessing import StandardScaler
+    taxonomy_dataframe_P = pd.DataFrame()
+    for i in all_ids:
+        #print(taxonomy_dataframe_P)
+        sample = pd.read_table(f'bracken_{tax_level}/{samples_map[i]}.bracken',index_col=0)
+        sample = sample[['fraction_total_reads']]
+        sample.columns = [i]
+        if len(taxonomy_dataframe_P.columns) == 0:
+            print('....')
+            taxonomy_dataframe_P = sample.copy()
+        else:
+            taxonomy_dataframe_P = pd.merge(taxonomy_dataframe_P, sample[[i]], how='outer', suffixes=("", ""),left_index=True, right_index=True)
+    #taxonomy_dataframe_P['phylum'] = taxonomy_dataframe_P.index
+    taxonomy_dataframe_P.fillna(0.0,inplace=True)
+    taxonomy_dataframe_P.index.rename('phylum',inplace=True)
+    taxonomy_dataframe_P['mean'] = taxonomy_dataframe_P.apply(lambda x: np.average(x),axis=1)
+    taxonomy_dataframe_P.sort_values(by='mean',ascending=False,inplace=True)
+    print(len(taxonomy_dataframe_P))
+    taxonomy_dataframe_P.drop('mean',axis=1,inplace=True)
+    taxonomy_dataframe_P = taxonomy_dataframe_P.loc[(taxonomy_dataframe_P == 0.0).mean(axis=1) < 0.9]
+    print(len(taxonomy_dataframe_P))
+    return taxonomy_dataframe_P
+
+def scale_dataframe(taxonomy_dataframe_P):
+    cols = taxonomy_dataframe_P.columns
+    ind = taxonomy_dataframe_P.index
+    scaler = StandardScaler()
+    taxonomy_dataframe_P = scaler.fit_transform(taxonomy_dataframe_P)
+    taxonomy_dataframe_P = pd.DataFrame(taxonomy_dataframe_P)
+    taxonomy_dataframe_P.columns = cols
+    taxonomy_dataframe_P.index = ind
+    return taxonomy_dataframe_P
+
+def plot_barplot(data,group_map):
+    fig, taxonomy_bar = plt.subplots()
+    #phylum_table_kraken = data
+    #phylum_table_kraken
+    #data = data.T.sort_values(by=['Actinobacteria','Bacteroidetes'],ascending=[False,False]).T
+    data.T.plot(ax=taxonomy_bar, kind="bar", stacked=True)
+    taxonomy_bar.legend(list(data.index)[:5],loc=1)
+    taxonomy_bar.set_xticklabels([group_map[i] for i in data.columns])
+    taxonomy_bar.set_xlabel('Samples')
+    taxonomy_bar.set_ylabel('Fraction of taxa in the system')
+    return fig
+
+def plot_pca(data,samples_a):
+    from sklearn.decomposition import PCA
+    from sklearn.preprocessing import StandardScaler
+    pcagenus = PCA(n_components=2)
+    pcagenus.fit(data)
+    var1, var2 = pcagenus.explained_variance_ratio_
+    fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
+#    ax=fig.add_axes([0,0,1,1])
+    ax.scatter(pcagenus.components_[0][samples_a:], pcagenus.components_[1][samples_a:],linewidths=2,edgecolors='#666666',s=300,color=sns.color_palette()[0],label='Treatment')
+    ax.scatter(pcagenus.components_[0][:samples_a], pcagenus.components_[1][:samples_a],linewidths=2,edgecolors='#666666',s=300,color=sns.color_palette()[1],label='Control')
+
+    confidence_ellipse(pcagenus.components_[0][samples_a:], pcagenus.components_[1][samples_a:],ax,n_std=3,edgecolor=sns.color_palette()[0])
+    confidence_ellipse(pcagenus.components_[0][:samples_a], pcagenus.components_[1][:samples_a],ax,n_std=3,edgecolor=sns.color_palette()[1])
+
+    ax.legend()
+    ax.set_xlabel(f'PC1 {round(100*var1,2)}%')
+    ax.set_ylabel(f'PC2 {round(100*var2,2)}%')
+    plt.tight_layout()
+    return fig
+
+def build_model(data,group_map):
+    from scipy import interp
+    from sklearn.datasets import make_classification
+    from sklearn.model_selection import RepeatedStratifiedKFold
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import roc_curve
+    from sklearn.metrics import plot_roc_curve
+    from sklearn.metrics import auc
+
+    phylum_feature_table = data.T
+    #phylum_feature_table['location'] = phylum_feature_table.index.map(lambda x: 1 if group_map[x] == 'Rosja' else 0)
+    
+    features = list(phylum_feature_table.columns[:-1])
+    target = phylum_feature_table.columns[-1]
+    X, y = np.array(phylum_feature_table[features]), np.array(phylum_feature_table[target])
+    #print(X[[3,5,7]])
+    #print(y.shape)
+
+    cv = RepeatedStratifiedKFold(n_splits=5,n_repeats=10)
+    classifier = LogisticRegression()
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+
+    fig, ax = plt.subplots()
+    for i, (train, test) in enumerate(cv.split(X, y)):
+        classifier.fit(X[train], y[train])
+        #print(y[test])
+        viz = plot_roc_curve(classifier, X[test], y[test],
+                             label='_nolegend_',
+                             alpha=0.0, lw=1, ax=ax)
+        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+
+
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color=sns.color_palette()[1],
+            label='Random', alpha=.8)
+
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color=sns.color_palette()[0],
+            label=r'Average ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. deviation')
+
+    ax.set_xlabel('1 - specificity')
+    ax.set_ylabel('sensitivity')
+
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+           title="")
+    ax.legend(loc="lower right")
+
+    return fig, classifier, features
+
+def get_top_features(classifier, features):
+
+    feature_importance = abs(classifier.coef_[0])
+    feature_importance = 100.0 * (feature_importance / feature_importance.max())
+    sorted_idx = np.argsort(feature_importance)
+    pos = np.arange(sorted_idx.shape[0]) + .5
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.barh(pos[-5:], feature_importance[sorted_idx][-5:], align='center', )
+    ax.set_yticks(pos[-5:])
+    ax.set_yticklabels(np.array(features)[sorted_idx][-5:], fontsize=16)
+    ax.set_xlabel('Top 5 features')
+    #plt.ylim(39,)
+    plt.tight_layout()
+    return fig
